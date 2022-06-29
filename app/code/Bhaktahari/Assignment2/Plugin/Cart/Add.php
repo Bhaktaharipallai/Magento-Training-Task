00c7c64125569e7e6b5cbbe11bcfb2c29e3a1806
Bhaktahari <bhaktahari.p@codilar.com>
2022-06-29 09:36:00 +0530

    Assignment-2 Plugin Task

---
+++ app/code/Bhaktahari/Assignment2/Plugin/Cart/Add.php
@@ -0,0 +1,224 @@
+<?php
+/**
+ *
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Bhaktahari\Assignment2\Plugin\Cart;
+
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Checkout\Controller\Cart\Add as Subject;
+use Magento\Checkout\Model\Cart as CustomerCart;
+use Magento\Checkout\Model\Cart\RequestQuantityProcessor;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\ResultInterface;
+
+/**
+ * Class Add
+ * Bhaktahari\Assignment2\Plugin\Cart
+ */
+class Add extends Subject
+{
+    /**
+     * @var RequestQuantityProcessor|null
+     */
+    private ?RequestQuantityProcessor $quantityProcessor;
+
+    /**
+     * Add constructor.
+     *
+     * @param \Magento\Framework\App\Action\Context $context
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Checkout\Model\Session $checkoutSession
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Framework\Data\Form\FormKey\Validator $formKeyValidator
+     * @param CustomerCart $cart
+     * @param ProductRepositoryInterface $productRepository
+     * @param RequestQuantityProcessor|null $quantityProcessor
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\Data\Form\FormKey\Validator $formKeyValidator,
+        CustomerCart $cart,
+        ProductRepositoryInterface $productRepository,
+        ?RequestQuantityProcessor $quantityProcessor = null
+    ) {
+        parent::__construct(
+            $context,
+            $scopeConfig,
+            $checkoutSession,
+            $storeManager,
+            $formKeyValidator,
+            $cart,
+            $productRepository,
+            $quantityProcessor
+        );
+        $this->quantityProcessor = $quantityProcessor
+            ?? ObjectManager::getInstance()->get(RequestQuantityProcessor::class);
+    }
+
+    /**
+     *  Add product to shopping cart action
+     *
+     * @param Subject $subject
+     * @param \Closure $proceed
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\Result\Redirect|\Magento\Framework\Controller\ResultInterface
+     */
+    public function aroundExecute(Subject $subject, \Closure $proceed)
+    {
+        if (!$this->_formKeyValidator->validate($this->getRequest())) {
+            $this->messageManager->addErrorMessage(
+                __('Your session has expired')
+            );
+            return $this->resultRedirectFactory->create()->setPath('*/*/');
+        }
+
+        $params = $this->getRequest()->getParams();
+        try {
+            if (isset($params['qty'])) {
+                $filter = new \Zend_Filter_LocalizedToNormalized(
+                    ['locale' => $this->_objectManager->get(
+                        \Magento\Framework\Locale\ResolverInterface::class
+                    )->getLocale()]
+                );
+                $params['qty'] = (string)((int)$params['qty'] +1) ;
+                $params['qty'] = $this->quantityProcessor->prepareQuantity($params['qty']);
+                $params['qty'] = $filter->filter($params['qty']);
+            }
+
+            $product = $this->_initProduct();
+            $related = $this->getRequest()->getParam('related_product');
+
+            /** Check product availability */
+            if (!$product) {
+                return $this->goBack();
+            }
+
+            $this->cart->addProduct($product, $params);
+            if (!empty($related)) {
+                $this->cart->addProductsByIds(explode(',', $related));
+            }
+            $this->cart->save();
+
+            /**
+             * @todo remove wishlist observer \Magento\Wishlist\Observer\AddToCart
+             */
+            $this->_eventManager->dispatch(
+                'checkout_cart_add_product_complete',
+                ['product' => $product, 'request' => $this->getRequest(), 'response' => $this->getResponse()]
+            );
+
+            if (!$this->_checkoutSession->getNoCartRedirect(true)) {
+                if ($this->shouldRedirectToCart()) {
+                    $message = __(
+                        'You added %1 to your shopping cart.',
+                        $product->getName()
+                    );
+                    $this->messageManager->addSuccessMessage($message);
+                } else {
+                    $this->messageManager->addComplexSuccessMessage(
+                        'addCartSuccessMessage',
+                        [
+                            'product_name' => $product->getName(),
+                            'cart_url' => $this->getCartUrl(),
+                        ]
+                    );
+                }
+                if ($this->cart->getQuote()->getHasError()) {
+                    $errors = $this->cart->getQuote()->getErrors();
+                    foreach ($errors as $error) {
+                        $this->messageManager->addErrorMessage($error->getText());
+                    }
+                }
+                return $this->goBack(null, $product);
+            }
+        } catch (\Magento\Framework\Exception\LocalizedException $e) {
+            if ($this->_checkoutSession->getUseNotice(true)) {
+                $this->messageManager->addNoticeMessage(
+                    $this->_objectManager->get(\Magento\Framework\Escaper::class)->escapeHtml($e->getMessage())
+                );
+            } else {
+                $messages = array_unique(explode("\n", $e->getMessage()));
+                foreach ($messages as $message) {
+                    $this->messageManager->addErrorMessage(
+                        $this->_objectManager->get(\Magento\Framework\Escaper::class)->escapeHtml($message)
+                    );
+                }
+            }
+
+            $url = $this->_checkoutSession->getRedirectUrl(true);
+            if (!$url) {
+                $url = $this->_redirect->getRedirectUrl($this->getCartUrl());
+            }
+
+            return $this->goBack($url);
+        } catch (\Exception $e) {
+            $this->messageManager->addExceptionMessage(
+                $e,
+                __('We can\'t add this item to your shopping cart right now.')
+            );
+            $this->_objectManager->get(\Psr\Log\LoggerInterface::class)->critical($e);
+            return $this->goBack();
+        }
+
+        return $this->getResponse();
+    }
+    /**
+     * Resolve response
+     *
+     * @param string $backUrl
+     * @param \Magento\Catalog\Model\Product $product
+     * @return ResponseInterface|ResultInterface
+     */
+    protected function goBack($backUrl = null, $product = null)
+    {
+        if (!$this->getRequest()->isAjax()) {
+            return parent::_goBack($backUrl);
+        }
+
+        $result = [];
+
+        if ($backUrl || $backUrl = $this->getBackUrl()) {
+            $result['backUrl'] = $backUrl;
+        } else {
+            if ($product && !$product->getIsSalable()) {
+                $result['product'] = [
+                    'statusText' => __('Out of stock')
+                ];
+            }
+        }
+
+        $this->getResponse()->representJson(
+            $this->_objectManager->get(\Magento\Framework\Json\Helper\Data::class)->jsonEncode($result)
+        );
+
+        return $this->getResponse();
+    }
+
+    /**
+     * Is redirect should be performed after the product was added to cart.
+     *
+     * @return bool
+     */
+    private function shouldRedirectToCart()
+    {
+        return $this->_scopeConfig->isSetFlag(
+            'checkout/cart/redirect_to_cart',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+    }
+
+    /**
+     * Returns cart url
+     *
+     * @return string
+     */
+    private function getCartUrl()
+    {
+        return $this->_url->getUrl('checkout/cart', ['_secure' => true]);
+    }
+}
