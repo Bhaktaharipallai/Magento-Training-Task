8673fb0e5986a46dec2d1942f4b6a83f354c6735
Bhaktahari <bhaktahari.p@codilar.com>
2022-07-04 10:32:45 +0530

    Assignment2 Using Model

---
+++ app/code/Bhaktahari/Assignment2/Plugin/Cart.php
@@ -0,0 +1,23 @@
+<?php
+namespace Bhaktahari\Assignment2\Plugin;
+
+/**
+ * Class Cart
+ * Bhaktahari\Assignment2\Plugin
+ */
+class Cart
+{
+    /**
+     * Update the quantity before add product
+     *
+     * @param \Magento\Checkout\Model\Cart $subject
+     * @param int|Product $productInfo $productInfo
+     * @param \Magento\Framework\DataObject|int|array $requestInfo
+     * @return array
+     */
+    public function beforeAddProduct(\Magento\Checkout\Model\Cart $subject, $productInfo, $requestInfo = null)
+    {
+        $requestInfo['qty'] += 1;
+        return [$productInfo,$requestInfo];
+    }
+}
